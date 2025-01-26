1eb2867972deddafa7ca9a9dbe05888eaebfeafd
kjungw1025 <kjungw1025@naver.com>
2025-01-27 00:29:21 +0900

    feat: JpaConfig 및 DTO 추가

---
+++ java/grpc/server/src/main/java/grpc/server/global/model/dto/ResponseIdDto.java
@@ -0,0 +1,10 @@
+package grpc.server.global.model.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResponseIdDto {
+    private final Long id;
+}
